703a1a43f0c62b7853ec640ca2ebab527a57d7c5
aytanbayramova <112419467+aytanbayramova@users.noreply.github.com>
2023-08-15 23:43:59 +0400

    slidebar was created

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import { useMode } from "./theme";
+import { ThemeProvider } from '@mui/material/styles';
+
+import Team from "./scenes/team";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import { CssBaseline, ThemeProvider } from "@mui/material";
+// import { ColorModeContext, useMode } from "./theme";
+// import Calendar from "./scenes/calendar/calendar";
+
+function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
+  return (
+    // <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        {/* <CssBaseline /> */}
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              {/* <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/calendar" element={<Calendar />} /> */}
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    // </ColorModeContext.Provider>
+  );
+}
+
+export default App;
